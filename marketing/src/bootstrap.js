05808a8b2374d902034235e78f9aec53fb7109d5
reinivaltanen <walty00@gmail.com>
2021-04-11 08:53:08 +0300

    wiring react

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Mount function to start up the app
+const mount = (el) => {
+    ReactDOM.render(
+        <h1>Hi there!</h1>,
+        el
+    );
+};
+
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.getElementById('_marketing-dev-root');
+
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+
+
+// We are running through container
+// and we should export the mount function
+export { mount };
