379a763080663d3fe133b59b3c68ecc20c86c4aa
reinivaltanen <walty00@gmail.com>
2021-04-11 15:02:29 +0300

    added a comma to trigger action

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -18,7 +18,7 @@ const devConfig = {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
             },
             shared: packageJson.dependencies
-        })
+        }),
     ]
 };
 
