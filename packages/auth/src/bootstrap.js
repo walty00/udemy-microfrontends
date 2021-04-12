67d156b01e1515285f79a0069f94199512efaa9b
reinivaltanen <walty00@gmail.com>
2021-04-12 19:36:25 +0300

    started implementing auth

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+// Mount function to start up the app
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate({ pathname: nextPathName }) {
+            const { pathname } = history.location;
+
+            if (pathname != nextPathName) {
+                history.push(nextPathName);
+            }
+        }
+    }
+};
+
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.getElementById('_auth-dev-root');
+
+    if (devRoot) {
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
+    }
+}
+
+
+
+// We are running through container
+// and we should export the mount function
+export { mount };
