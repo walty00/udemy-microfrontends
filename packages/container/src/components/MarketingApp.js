fe1bf7c5b883f9fb15dcab5fa26b9b2d9d16c37a
reinivaltanen <walty00@gmail.com>
2021-04-12 19:15:55 +0300

    navigation communication between apps

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,20 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom'
 
 export default () => {
    const ref = useRef(null);
+   const history = useHistory();
 
    useEffect(() => {
-       mount(ref.current);
+       mount(ref.current, {
+           onNavigate: ({ pathname: nextPathName }) => {
+               const { pathname } = history.location;
+               if (pathname !== nextPathName) {
+                   history.push(nextPathName);
+               }
+           }
+       });
    });
 
    return <div ref={ref}/>;
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-    ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+    const history = createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history} />, el);
 };
 
 
@@ -14,7 +21,7 @@ if (process.env.NODE_ENV === 'development') {
     const devRoot = document.getElementById('_marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {});
     }
 }
 
