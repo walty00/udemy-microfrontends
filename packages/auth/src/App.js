ba8d552cdaf6d3ff3e62838fcf66bf5181cedb07
reinivaltanen <walty00@gmail.com>
2021-04-12 19:40:43 +0300

    added auth routes

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-// import Landing from './components/Landing';
-// import Pricing from './components/Pricing';
+import SignIn from './components/Signin';
+import SignUp from './components/Signup';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'au'
@@ -14,7 +14,10 @@ export default ({ history }) => {
         <div>
             <StylesProvider generateClassName={generateClassName}>
                 <Router history={history}>
-                    <Switch></Switch>
+                    <Switch>
+                        <Route path="/auth/signin" component={SignIn} />
+                        <Route path="/auth/signup" component={SignUp} />
+                    </Switch>
                 </Router>
             </StylesProvider>
         </div>
